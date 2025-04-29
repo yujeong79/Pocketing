31cfc45b283b15a73b319928ff223aa015969ad6
한병현 <gksqudgus777@naver.com>
2025-04-29 16:32:06 +0900

    FEAT: SignIn 화면 UI 구현

---
+++ FE/src/assets/assets.tsx
@@ -0,0 +1,6 @@
+import ThreeDLogo from '@/assets/icons/3d-logo.svg';
+import TextLogo from '@/assets/icons/pocketing-text-large.svg';
+import KakaoLoginButton from '@/assets/icons/kakao-login.svg';
+import XLoginButton from '@/assets/icons/x-login.svg';
+
+export { ThreeDLogo, TextLogo, KakaoLoginButton, XLoginButton };
