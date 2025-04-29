59b341ddd260c495570c83945f4896d70dc61cf9
한윤지 <hyj9908@gamil.com>
2025-04-29 11:05:30 +0900

    REFACTOR: entity 어노테이션 추가 기본생성자 추가 및 로그 생성자 추가

--- BE/src/main/java/com/a406/pocketing/member/entity/UserLikedMember.java
+++ BE/src/main/java/com/a406/pocketing/member/entity/UserLikedMember.java
@@ -1,10 +1,15 @@
 package com.a406.pocketing.member.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Slf4j
 @Table(
         name = "user_liked_member",
         indexes = {
