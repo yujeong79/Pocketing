d6bc1c48894f019ffb3bb2b9f35ef29309ad7e63
한병현 <gksqudgus777@naver.com>
2025-04-29 17:52:56 +0900

    FEAT: 화면 비율에 따른 item 크기 조정을 위한 scale 구현

--- FE/src/utils/scale.ts
+++ FE/src/utils/scale.ts
@@ -1,3 +1,8 @@
+const width = window.innerWidth;
+const scale = (size: number) => (width / 298) * size;
+
 export function scaleLetterSpacing(fontSize: number, percent: number): number {
   return parseFloat((fontSize * (percent / 100)).toFixed(2));
 }
+
+export default scale;
