a525146a3545ddc8959e814cbafd0397b2fdd103
juwon <juwon0404@nate.com>
2025-04-29 17:51:51 +0900

    CHORE: 프로젝트 전역 settings

---
+++ AI/app/config/settings.py
@@ -0,0 +1,37 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+
+    OPENAI_API_KEY: str
+
+    EMBEDDING_MODEL: str
+
+    WS_HOST: str
+    WS_PORT: int
+
+    BACKEND_API_URL:str
+
+    CHROMA_HOST: str
+    CHROMA_PORT: int
+    CHROMA_DB_PATH: str
+
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_DB: int
+
+    LLM_MODEL: str
+    LLM_TEMPERATURE: float
+
+    LOG_LEVEL: str
+
+    MONGO_URI: str
+    MONGO_DB_NAME: str
+    MONGO_COLLECTION: str
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        extra="ignore"
+    )
+
+settings = Settings()
