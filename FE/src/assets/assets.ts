0eec35bf1996c01f984ca78ac4c65109d1781f4c
이승희 <dearlsh1125@gmail.com>
2025-04-30 10:54:14 +0900

    CHORE: NavBar 아이콘 aseet에 추가

--- FE/src/assets/assets.ts
+++ FE/src/assets/assets.ts
@@ -4,6 +4,13 @@ import BackIcon from '@/assets/icons/back.svg';
 import LogoText from '@/assets/icons/logo-text.svg';
 import ModifyIcon from '@/assets/icons/modify.svg';
 
+// NavBar
+import MainIcon from '@/assets/icons/main.svg';
+import ChatIcon from '@/assets/icons/chat.svg';
+import ExchangeIcon from '@/assets/icons/exchange.svg';
+import PostIcon from '@/assets/icons/post.svg';
+import MyPageIcon from '@/assets/icons/mypage.svg';
+
 // Login
 import ThreeDLogo from '@/assets/icons/3d-logo.svg';
 import TextLogo from '@/assets/icons/pocketing-text-large.svg';
@@ -16,6 +23,12 @@ export {
   BackIcon,
   LogoText,
   ModifyIcon,
+  //NavBar
+  MainIcon,
+  ChatIcon,
+  ExchangeIcon,
+  PostIcon,
+  MyPageIcon,
   //Login
   ThreeDLogo,
   TextLogo,
