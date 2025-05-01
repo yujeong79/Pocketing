477357e7aff9dfcfcefd26bd06314071deea7b42
이승희 <dearlsh1125@gmail.com>
2025-05-02 02:13:22 +0900

    STYLE: 스크롤 시 navbar에 가려지는 부분 수정

--- FE/src/components/layout/Layout.tsx
+++ FE/src/components/layout/Layout.tsx
@@ -1,11 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import NavBar from '../common/NavBar/NavBar';
+import styled from 'styled-components';
+import scale from '@/utils/scale';
+
+const LayoutContainer = styled.div`
+  padding-bottom: ${scale(72)}px;
+`;
 
 export default function Layout() {
   return (
-    <>
+    <LayoutContainer>
       <Outlet />
       <NavBar />
-    </>
+    </LayoutContainer>
   );
 }
