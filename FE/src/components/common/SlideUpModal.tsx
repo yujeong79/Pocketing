7920c2b56535be2931b52c2d163e00bbbd5e1fb1
한병현 <gksqudgus777@naver.com>
2025-05-01 18:41:23 +0900

    FEAT: 공통 컴포넌트 Slide up modal 구현

---
+++ FE/src/components/common/SlideUpModal.tsx
@@ -0,0 +1,42 @@
+import { useEffect, useState } from 'react';
+
+import * as S from './SlideUpModalStyle';
+import { CloseIcon } from '@/assets/assets';
+
+interface SlideUpModalProps {
+  header: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children: React.ReactNode;
+}
+
+const SlideUpModal = ({ header, isOpen, onClose, children }: SlideUpModalProps) => {
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    if (isOpen) {
+      setIsAnimating(true);
+    } else {
+      const timer = setTimeout(() => {
+        setIsAnimating(false);
+      }, 300);
+      return () => clearTimeout(timer);
+    }
+  }, [isOpen]);
+
+  if (!isOpen && !isAnimating) return null;
+
+  return (
+    <S.Overlay $isOpen={isOpen} onClick={onClose}>
+      <S.ModalContainer $isOpen={isOpen} onClick={(e) => e.stopPropagation()}>
+        <S.ModalHeader>
+          <S.ModalTitle>{header}</S.ModalTitle>
+          <S.CloseButton src={CloseIcon} onClick={onClose} />
+        </S.ModalHeader>
+        <S.ModalContent>{children}</S.ModalContent>
+      </S.ModalContainer>
+    </S.Overlay>
+  );
+};
+
+export default SlideUpModal;
