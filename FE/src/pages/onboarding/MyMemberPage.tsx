65e7c0be1885f2bd4ee45a4e44cc16edb12b65af
한병현 <gksqudgus777@naver.com>
2025-05-01 14:19:53 +0900

    FEAT: 멤버 선택 페이지 UI 구현

--- FE/src/pages/onboarding/MyMemberPage.tsx
+++ FE/src/pages/onboarding/MyMemberPage.tsx
@@ -1,5 +1,48 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { useState } from 'react';
+
+import * as S from './MyMemberStyle';
+import BackButton from '@/pages/onboarding/components/BackButton';
+import Button from '@/components/common/Button';
+import { artistList } from '@/mocks/artist';
+import MemberItem from './components/MemberItem';
+
 const MyMemberPage = () => {
-  return <div>MyMemberPage</div>;
+  const navigate = useNavigate();
+  const { groupId } = useParams();
+  const [selectedMembers, setSelectedMembers] = useState<string[]>([]);
+
+  const selectedGroup = artistList.find((artist) => artist.groupId === Number(groupId));
+
+  if (!selectedGroup) {
+    return null;
+  }
+
+  const handleMemberClick = (member: string) => {
+    setSelectedMembers((prev) =>
+      prev.includes(member) ? prev.filter((name) => name !== member) : [...prev, member]
+    );
+  };
+
+  return (
+    <S.PageContainer>
+      <S.ItemContainer>
+        <BackButton />
+        <S.Title>관심 멤버를 선택해주세요</S.Title>
+        <S.MemberListContainer>
+          {selectedGroup.members.map((member) => (
+            <MemberItem
+              key={member}
+              member={member}
+              isSelected={selectedMembers.includes(member)}
+              onPressed={() => handleMemberClick(member)}
+            />
+          ))}
+        </S.MemberListContainer>
+      </S.ItemContainer>
+      <Button text="다음" onClick={() => navigate('/signup/complete')} />
+    </S.PageContainer>
+  );
 };
 
 export default MyMemberPage;
---
+++ FE/src/pages/onboarding/MyMemberStyle.ts
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+
+import { FontStyles } from '@/constants/fonts';
+import scale from '@/utils/scale';
+import { colors } from '@/styles/theme';
+
+export const PageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  min-height: 100vh;
+  padding-top: ${scale(24)}px;
+  padding-left: ${scale(16)}px;
+  padding-right: ${scale(16)}px;
+  padding-bottom: ${scale(64)}px;
+`;
+
+export const ItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Title = styled.div`
+  padding-top: ${scale(60)}px;
+  padding-bottom: ${scale(36)}px;
+  text-align: left;
+
+  ${FontStyles.headingMedium};
+  color: ${colors.gray800};
+`;
+
+export const MemberListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: ${scale(280)}px;
+  gap: ${scale(24)}px;
+
+  overflow-y: auto;
+  -ms-overflow-style: none;
+  scrollbar-width: none;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const MemberItem = styled.div<{ $isSelected: boolean }>`
+  display: flex;
+  flex-direction: column;
+  gap: ${scale(24)}px;
+
+  text-align: left;
+  ${FontStyles.headingLarge};
+  color: ${(props) => (props.$isSelected ? colors.primary : colors.primary200)};
+  cursor: pointer;
+
+  &:hover {
+    color: ${colors.primary};
+  }
+`;
+
+export const SelectedMemberContainer = styled.div`
+  text-align: left;
+  ${FontStyles.headingLarge};
+  color: ${colors.primary};
+`;
+
+export const SelectedIcon = styled.img`
+  width: ${scale(24)}px;
+  height: ${scale(32)}px;
+`;
