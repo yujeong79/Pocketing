d6cbcee444226fb6d85920eef246a539db54809c
이승희 <dearlsh1125@gmail.com>
2025-05-02 16:33:19 +0900

    FEAT: height 및 fontStyle props 추가

--- FE/src/components/common/ButtonStyle.ts
+++ FE/src/components/common/ButtonStyle.ts
@@ -1,17 +1,26 @@
 import styled from 'styled-components';
 import { theme } from '@/styles/theme';
-import { FontStyles } from '@/constants/fonts';
+import { FontStyles } from '@/constants/fonts'; // FontStyles import
 import scale from '@/utils/scale';
 
-export const ButtonContainer = styled.button`
+interface ButtonContainerProps {
+  height?: number;
+  fontStyle?: keyof typeof FontStyles; // FontStyles에서 타입을 가져옵니다.
+}
+
+export const ButtonContainer = styled.button<ButtonContainerProps>`
   cursor: pointer;
   width: ${scale(266)}px;
-  height: ${scale(48)}px;
+  height: ${(props) =>
+    props.height
+      ? `${scale(props.height)}px`
+      : `${scale(48)}px`}; /* height prop에 따라 동적으로 설정 */
   background-color: ${theme.colors.primary};
   border-radius: ${scale(10)}px;
   border: none;
 
-  ${FontStyles.headingMedium};
+  /* FontStyles 동적으로 변경 */
+  ${(props) => (props.fontStyle ? FontStyles[props.fontStyle] : FontStyles.headingMedium)};
   color: ${theme.colors.white};
   text-align: center;
 `;
