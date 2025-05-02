12aed7e3422bd63b8c2a56a1a2690a36a2b2c76f
이승희 <dearlsh1125@gmail.com>
2025-05-02 16:37:38 +0900

    FEAT: 게시글 상세 페이지 및 스타일 컴포넌트 추가

---
+++ FE/src/pages/main/DetailPageStyle.ts
@@ -0,0 +1,68 @@
+import { FontStyles } from '@/constants/fonts';
+import { colors } from '@/styles/theme';
+import scale from '@/utils/scale';
+import styled from 'styled-components';
+
+export const DetailPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: calc(100vh - ${scale(128)}px);
+`;
+
+export const ContentSection = styled.div`
+  flex: 1;
+  overflow-y: hidden;
+`;
+
+export const GraySection = styled.div`
+  width: 100%;
+  min-height: ${scale(364)}px;
+  background-color: ${colors.gray100};
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 ${scale(16)}px;
+`;
+
+export const InformationText = styled.p`
+  ${FontStyles.captionSmall};
+  color: ${colors.gray600};
+  white-space: pre-line;
+  text-align: center;
+  margin-top: ${scale(12)}px;
+  margin-bottom: ${scale(12)}px;
+`;
+
+export const PhotoCardImage = styled.img`
+  width: ${scale(168)}px;
+  height: ${scale(258)}px;
+  object-fit: cover;
+  border-radius: ${scale(4)}px;
+`;
+
+export const ChipsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: ${scale(8)}px;
+  flex-wrap: wrap;
+`;
+
+export const SellerSection = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const ChatButton = styled.div`
+  margin-top: ${scale(8)}px;
+  margin-bottom: ${scale(24)}px;
+`;
+
+export const ButtonWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-bottom: ${scale(24)}px;
+`;
