02a6bf5abb6b5b7609d8b61de0fea02b80eb13f0
한병현 <gksqudgus777@naver.com>
2025-05-02 17:00:49 +0900

    FEAT: Mock data 추가

---
+++ FE/src/types/mySale.ts
@@ -0,0 +1,11 @@
+export interface MySale {
+  id: number;
+  group: string;
+  member: string;
+  album: string;
+  version: string;
+  date: string;
+  price: number;
+  image: string;
+  sale: boolean;
+}
