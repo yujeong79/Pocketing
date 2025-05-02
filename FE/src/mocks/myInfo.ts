fcee84af03deb18a904b4014be8ecaab341903c4
한병현 <gksqudgus777@naver.com>
2025-05-02 09:49:58 +0900

    FEAT: 내 정보 mock data 추가

---
+++ FE/src/mocks/myInfo.ts
@@ -0,0 +1,11 @@
+import { MyInfo } from '@/types/myInfo';
+
+export const myInfo: MyInfo = {
+  name: '한병현',
+  nickname: '포카캌',
+  profileImageUrl: 'https://cataas.com/cat/orange,cute',
+  isVerified: true,
+  address: '서울특별시 강남구 테헤란로 14길 6 남도빌딩 2층',
+  bank: '농협',
+  account: '11012345678901234567',
+};
