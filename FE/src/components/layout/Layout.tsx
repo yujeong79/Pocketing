74db9a76688641b2f110a8d0b22b1ae52b1dc790
이승희 <dearlsh1125@gmail.com>
2025-05-05 18:22:10 +0900

    CHORE: 레이아웃에 챗봇 버튼 컴포넌트 추가

--- FE/src/components/layout/Layout.tsx
+++ FE/src/components/layout/Layout.tsx
@@ -2,6 +2,7 @@ import { Outlet } from 'react-router-dom';
 import NavBar from '../common/NavBar/NavBar';
 import styled from 'styled-components';
 import scale from '@/utils/scale';
+import ChatbotButton from '../chatbot/ChatbotButton';
 
 const LayoutContainer = styled.div`
   padding-bottom: ${scale(32)}px;
@@ -11,6 +12,7 @@ export default function Layout() {
   return (
     <LayoutContainer>
       <Outlet />
+      <ChatbotButton />
       <NavBar />
     </LayoutContainer>
   );
