8a354632d26051d5fb6f49d866c23547345ed1f4
이승희 <dearlsh1125@gmail.com>
2025-05-07 17:37:41 +0900

    FEAT: 거래 상태 변경 모달 추가

--- FE/src/pages/message/components/TradeItem.tsx
+++ FE/src/pages/message/components/TradeItem.tsx
@@ -1,8 +1,13 @@
 import { mockChat } from '@/mocks/chat';
 import * as S from './TradeItemStyle';
+import { useState } from 'react';
+import StateModal from './StateModal';
+
+type Status = 'AVAILABLE' | 'COMPLETED';
 
 const TradeItem = () => {
-  const status = mockChat.result.linkedPost.status;
+  const [status, setStatus] = useState<Status>(mockChat.result.linkedPost.status as Status);
+  const [isStateModalOpen, setIsStateModalOpen] = useState(false);
   const isAvailable = status === 'AVAILABLE';
 
   const formattedPrice = mockChat.result.linkedPost.price.toLocaleString();
@@ -13,29 +18,42 @@ const TradeItem = () => {
 
   const handleClickStateButton = (e: React.MouseEvent) => {
     e.stopPropagation(); // 클릭 이벤트 전파 방지
-    console.log('상태 버튼 클릭');
+    setIsStateModalOpen(true);
+  };
+
+  const handleStateSelect = (newStatus: Status) => {
+    setStatus(newStatus);
   };
 
   return (
-    <S.Container onClick={handleClickTradeItem}>
-      <S.ProfileSection>
-        <S.ProfileImage src={mockChat.result.linkedPost.photocard.cardImageUrl} />
-      </S.ProfileSection>
+    <>
+      <S.Container onClick={handleClickTradeItem}>
+        <S.ProfileSection>
+          <S.ProfileImage src={mockChat.result.linkedPost.photocard.cardImageUrl} />
+        </S.ProfileSection>
 
-      <S.InfoSection>
-        <S.StateButtonContainer>
-          <S.StateButton isAvailable={isAvailable} onClick={handleClickStateButton}>
-            {isAvailable ? '판매중' : '거래완료'}
-            <S.Arrow>&gt;</S.Arrow>
-          </S.StateButton>
-        </S.StateButtonContainer>
+        <S.InfoSection>
+          <S.StateButtonContainer>
+            <S.StateButton isAvailable={isAvailable} onClick={handleClickStateButton}>
+              {isAvailable ? '판매중' : '거래완료'}
+              <S.Arrow>&gt;</S.Arrow>
+            </S.StateButton>
+          </S.StateButtonContainer>
 
-        <S.AlbumTitle>{mockChat.result.linkedPost.photocard.albumTitle}</S.AlbumTitle>
-      </S.InfoSection>
+          <S.AlbumTitle>{mockChat.result.linkedPost.photocard.albumTitle}</S.AlbumTitle>
+        </S.InfoSection>
 
-      {/* 가격 섹션을 오른쪽 끝으로 배치 */}
-      <S.PriceSection>{formattedPrice} 원</S.PriceSection>
-    </S.Container>
+        {/* 가격 섹션을 오른쪽 끝으로 배치 */}
+        <S.PriceSection>{formattedPrice} 원</S.PriceSection>
+      </S.Container>
+
+      <StateModal
+        isOpen={isStateModalOpen}
+        onClose={() => setIsStateModalOpen(false)}
+        onSelect={handleStateSelect}
+        currentStatus={status}
+      />
+    </>
   );
 };
 
