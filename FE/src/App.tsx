a2271f7eee9faafd2e618e18a11f6d9530d7342c
taemin <ktm990323@naver.com>
2025-05-08 16:20:47 +0900

    FIX: fcm 초기 설정 수정
    S12P31A406-185

--- FE/src/App.tsx
+++ FE/src/App.tsx
@@ -1,11 +1,30 @@
+import { useEffect } from 'react';
 import { RouterProvider } from 'react-router-dom';
 import router from './router/router';
 import './App.css';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './styles/theme';
 import { GlobalStyle } from './styles/GlobalStyle';
+import { requestFcmToken } from '@/firebase';
+import { registerFcmToken } from '@/fcm';
 
 function App() {
+  useEffect(() => {
+    const initFcm = async () => {
+      const permission = await Notification.requestPermission();
+      if (permission !== 'granted') {
+        console.warn('❌ 알림 권한 거부됨');
+        return;
+      }
+
+      const token = await requestFcmToken();
+      if (token) {
+        await registerFcmToken(token);
+      }
+    };
+
+    initFcm();
+  }, []);
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
--- FE/vite.config.ts
+++ FE/vite.config.ts
@@ -10,4 +10,18 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        sw: path.resolve(__dirname, 'public/firebase-messaging-sw.ts'), // 서비스워커 추가
+      },
+      output: {
+        entryFileNames: (chunkInfo) => {
+          if (chunkInfo.name === 'sw') return 'firebase-messaging-sw.js'; //ts → js 변환
+          return '[name].js';
+        }
+      }
+    }
+  }
 });
