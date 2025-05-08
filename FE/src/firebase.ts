70cdef23a2a79db810f4a51b9ed1bddbc1f81bd8
이승희 <dearlsh1125@gmail.com>
2025-05-08 17:32:35 +0900

    FIX: 타입 수정 및 firebase 설치

--- FE/src/firebase.ts
+++ FE/src/firebase.ts
@@ -1,47 +1,47 @@
 // src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getMessaging, getToken, onMessage } from "firebase/messaging";
+import { initializeApp } from 'firebase/app';
+import { getMessaging, getToken, onMessage } from 'firebase/messaging';
 
 // Firebase 콘솔에서 복사한 config
 const firebaseConfig = {
-  apiKey: "AIzaSyBY_KbODG9HQzyltb_oSt-Oh1bD77Z3qlk",
-  authDomain: "pocketing-7a2dd.firebaseapp.com",
-  projectId: "pocketing-7a2dd",
-  storageBucket: "pocketing-7a2dd.appspot.com",
-  messagingSenderId: "869118637468",
-  appId: "1:869118637468:web:591f35eff6ce9fce210459",
-  measurementId: "G-ZBLL446HH9"
+  apiKey: 'AIzaSyBY_KbODG9HQzyltb_oSt-Oh1bD77Z3qlk',
+  authDomain: 'pocketing-7a2dd.firebaseapp.com',
+  projectId: 'pocketing-7a2dd',
+  storageBucket: 'pocketing-7a2dd.appspot.com',
+  messagingSenderId: '869118637468',
+  appId: '1:869118637468:web:591f35eff6ce9fce210459',
+  measurementId: 'G-ZBLL446HH9',
 };
 
 const app = initializeApp(firebaseConfig);
 const messaging = getMessaging(app);
 
 // 🟡 FCM 토큰 요청 함수
-export const requestFcmToken = async (): Promise<String | null> => {
+export const requestFcmToken = async (): Promise<string | null> => {
   try {
     const token = await getToken(messaging, {
-      vapidKey: "BIULqTtkq1GzlTHMjOzncSv_GsJJE36fuyKGR0pCSDNQtLuk2fIiUxObTvw0uN9_AENBNAKhZ_DFrMVuNzZ5B_A", // 콘솔에서 발급받기
+      vapidKey:
+        'BIULqTtkq1GzlTHMjOzncSv_GsJJE36fuyKGR0pCSDNQtLuk2fIiUxObTvw0uN9_AENBNAKhZ_DFrMVuNzZ5B_A', // 콘솔에서 발급받기
     });
-      console.log("✅ FCM Token:", token);
-      return token;
+    console.log('✅ FCM Token:', token);
+    return token;
   } catch (err) {
-    console.error("❌ FCM 토큰 요청 실패:", err);
+    console.error('❌ FCM 토큰 요청 실패:', err);
     return null;
   }
 };
 
 // 🟡 포그라운드 메시지 수신 핸들러
 onMessage(messaging, (payload) => {
-  console.log("📩 포그라운드 알림 수신:", payload);
+  console.log('📩 포그라운드 알림 수신:', payload);
   const { title, body } = payload.notification || {};
 
-  if (Notification.permission === "granted" && title) {
+  if (Notification.permission === 'granted' && title) {
     new Notification(title, {
       body,
-      icon: "/pocketing.svg",
+      icon: '/pocketing.svg',
     });
   }
 });
-  
-  export default app;
-  
+
+export default app;
