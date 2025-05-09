089a82d02d4f682bb336dd1c27bc5324a354f080
이승희 <dearlsh1125@gmail.com>
2025-05-09 18:05:27 +0900

    FEAT: 앨범 및 관심 그룹 API와 타입 정의 추가

---
+++ FE/src/api/artist/album.ts
@@ -0,0 +1,8 @@
+import axiosInstance from '@/api/auth/axiosInstance';
+import { AlbumResponse } from '@/types/album';
+
+// 앨범 목록 조회
+export const getAlbums = async (groupId: number): Promise<AlbumResponse> => {
+  const response = await axiosInstance.get(`/albums?groupId=${groupId}`);
+  return response.data;
+};
---
+++ FE/src/api/user/like.ts
@@ -0,0 +1,32 @@
+import axiosInstance from '@/api/auth/axiosInstance';
+import { LikedGroupList, UserResponse } from '@/types/user';
+
+// 관심 그룹 전체 조회
+export const getLikedGroups = async (): Promise<UserResponse> => {
+  const response = await axiosInstance.get('/user/like/group');
+  return response.data;
+};
+
+// 관심 그룹 내 관심 멤버 조회
+export const getLikedMembers = async (groupId: number): Promise<UserResponse> => {
+  const response = await axiosInstance.get(`/user/like/member?groupId=${groupId}`);
+  return response.data;
+};
+
+// 관심 그룹 및 멤버 추가
+export const postLikedMembers = async (likedGroupList: LikedGroupList): Promise<UserResponse> => {
+  const response = await axiosInstance.post('/user/like/info', likedGroupList);
+  return response.data;
+};
+
+// 관심 그룹 삭제
+export const deleteLikedGroups = async (groupId: number): Promise<UserResponse> => {
+  const response = await axiosInstance.delete(`/user/like/group?groupId=${groupId}`);
+  return response.data;
+};
+
+// 관심 멤버 삭제
+export const deleteLikedMembers = async (memberId: number): Promise<UserResponse> => {
+  const response = await axiosInstance.delete(`/user/like/member?memberId=${memberId}`);
+  return response.data;
+};
