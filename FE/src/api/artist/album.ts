136fe1483e42e1151636839e4bca4dd76c6738e2
이승희 <dearlsh1125@gmail.com>
2025-05-10 00:24:30 +0900

    REFACTOR: 앨범 및 멤버 API 네이밍 수정

--- FE/src/api/artist/album.ts
+++ FE/src/api/artist/album.ts
@@ -2,7 +2,7 @@ import axiosInstance from '@/api/auth/axiosInstance';
 import { AlbumResponse } from '@/types/album';
 
 // 앨범 목록 조회
-export const getAlbums = async (groupId: number): Promise<AlbumResponse> => {
+export const fetchAlbums = async (groupId: number): Promise<AlbumResponse> => {
   const response = await axiosInstance.get(`/albums?groupId=${groupId}`);
   return response.data;
 };
