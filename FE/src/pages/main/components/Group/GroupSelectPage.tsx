be41ade0ec1ff8ae8a7f6b032420a2d605d11b71
이승희 <dearlsh1125@gmail.com>
2025-05-12 15:58:41 +0900

    FEAT: 그룹 선택 검색 기능 및 api 연동 로직 개선

--- FE/src/pages/main/components/Group/GroupSelectPage.tsx
+++ FE/src/pages/main/components/Group/GroupSelectPage.tsx
@@ -1,34 +1,58 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import * as S from './GroupSelectStyle';
 import BackButton from '@/pages/onboarding/components/BackButton';
 import Button from '@/components/common/Button';
 import { SearchIcon } from '@/assets/assets';
-import { artistList } from '@/mocks/artist';
+import { useGroups } from '@/hooks/artist/query/useGroups';
+import { useGroupSearch } from '@/hooks/search/useGroupSearch';
+import { Group } from '@/types/group';
 
 interface GroupSelectPageProps {
   onGroupSelect: (groupId: number | null) => void;
   selectedAllGroup: number | null;
+  selectedGroupData: Group | null;
 }
 
 const GroupSelectPage = ({ onGroupSelect, selectedAllGroup }: GroupSelectPageProps) => {
   const navigate = useNavigate();
-  const [tempSelectedGroup, setTempSelectedGroup] = useState<number | null>(selectedAllGroup);
+  const location = useLocation();
+  const { data: groupsData } = useGroups();
+  // 이전 선택 상태를 초기값으로 사용
+  const [tempSelectedGroup, setTempSelectedGroup] = useState<number | null>(
+    location.state?.previousSelectedAllGroup || selectedAllGroup
+  );
+
+  // 검색
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const { filteredGroups } = useGroupSearch({
+    groups: groupsData?.result,
+    searchTerm,
+  });
 
   const handleGroupSelect = (groupId: number) => {
     setTempSelectedGroup(groupId);
   };
 
   const handleConfirm = () => {
-    if (tempSelectedGroup) {
+    if (tempSelectedGroup && groupsData?.result) {
+      const selectedGroupData = groupsData?.result.find(
+        (group) => group.groupId === tempSelectedGroup
+      );
       onGroupSelect(tempSelectedGroup);
       navigate('/main', {
         replace: true,
-        state: { selectedAllGroup: tempSelectedGroup },
+        state: {
+          selectedAllGroup: tempSelectedGroup,
+          selectedGroupData: selectedGroupData, // 그룹 전체 정보 전달
+        },
       });
     }
   };
+
+
 
   return (
     <S.PageContainer>
@@ -36,18 +60,22 @@ const GroupSelectPage = ({ onGroupSelect, selectedAllGroup }: GroupSelectPagePro
         <BackButton />
         <S.Title>그룹을 선택해주세요</S.Title>
         <S.SearchContainer>
-          <S.SearchInput placeholder="그룹명을 검색해주세요" />
+          <S.SearchInput
+            placeholder="그룹명을 검색해주세요"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
           <S.SearchIcon src={SearchIcon} />
         </S.SearchContainer>
         <S.GroupListContainer>
-          {artistList.map((artist) => (
+          {filteredGroups.map((group) => (
             <S.GroupInfo
-              key={artist.groupId}
-              onClick={() => handleGroupSelect(artist.groupId)}
-              $isSelected={tempSelectedGroup === artist.groupId}
+              key={group.groupId}
+              onClick={() => handleGroupSelect(group.groupId)}
+              $isSelected={tempSelectedGroup === group.groupId}
             >
-              <S.GroupImage src={artist.image} />
-              <S.GroupName>{artist.name}</S.GroupName>
+              <S.GroupImage src={group.groupImageUrl} />
+              <S.GroupName>{group.groupNameKo}</S.GroupName>
             </S.GroupInfo>
           ))}
         </S.GroupListContainer>
--- FE/src/pages/profile/MyGroupEditPage.tsx
+++ FE/src/pages/profile/MyGroupEditPage.tsx
@@ -4,12 +4,61 @@ import * as S from './MyGroupEditStyle';
 import BackButton from '@/pages/onboarding/components/BackButton';
 import Button from '@/components/common/Button';
 import { SearchIcon } from '@/assets/assets';
-import { artistList } from '@/mocks/artist';
+import { useEffect, useState } from 'react';
+import { useLikedMembersStore } from '@/store/likedMembers';
+import { useGroupSearch } from '@/hooks/search/useGroupSearch';
+import { useGroups } from '@/hooks/artist/query/useGroups';
+import { useLikedGroups } from '@/hooks/user/query/useLike';
+import { useUpdateLikedMembers } from '@/hooks/user/mutation/useLike';
+import { UserLikedGroup } from '@/types/user';
 
 const MyGroupEditPage = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const fromPath = location.state?.from || '/profile';
+  // API 호출
+  const { data: groupsData } = useGroups();
+  const { data: likedGroupsData } = useLikedGroups();
+  const { mutate: updateLikedMembers } = useUpdateLikedMembers();
+
+  // 상태 관리
+  const [searchTerm, setSearchTerm] = useState('');
+  const { getSelectedMembers } = useLikedMembersStore();
+  const [selectedGroups, setSelectedGroups] = useState<number[]>([]);
+
+  // 기존 관심 그룹 정보 로드
+  useEffect(() => {
+    if (likedGroupsData?.result) {
+      const likedGroupIds = likedGroupsData.result
+        .filter((item): item is UserLikedGroup => 'groupId' in item)
+        .map((group) => group.groupId);
+      setSelectedGroups(likedGroupIds);
+    }
+  }, [likedGroupsData]);
+
+  const { filteredGroups } = useGroupSearch({
+    groups: groupsData?.result,
+    searchTerm,
+  });
+
+  // 그룹 선택 핸들러
+  const handleComplete = () => {
+    const likedGroupList = {
+      groupId: selectedGroups[0],
+      likedMemberList: getSelectedMembers(selectedGroups[0]).map((memberId) => ({
+        memberId,
+      })),
+    };
+
+    updateLikedMembers(likedGroupList, {
+      onSuccess: () => {
+        navigate(fromPath);
+      },
+      onError: (error) => {
+        console.error('관심 그룹 업데이트 실패', error);
+      },
+    });
+  };
 
   return (
     <S.PageContainer>
@@ -17,26 +66,30 @@ const MyGroupEditPage = () => {
         <BackButton />
         <S.Title>관심 그룹을 선택해주세요</S.Title>
         <S.SearchContainer>
-          <S.SearchInput placeholder="그룹명을 검색해주세요" />
+          <S.SearchInput
+            placeholder="그룹명을 검색해주세요"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
           <S.SearchIcon src={SearchIcon} />
         </S.SearchContainer>
         <S.GroupListContainer>
-          {artistList.map((artist) => (
+          {filteredGroups.map((group) => (
             <S.GroupInfo
-              key={artist.groupId}
+              key={group.groupId}
               onClick={() =>
-                navigate(`/myMemberEdit/${artist.groupId}`, {
+                navigate(`/myMemberEdit/${group.groupId}`, {
                   state: { from: fromPath },
                 })
               }
             >
-              <S.GroupImage src={artist.image} />
-              <S.GroupName>{artist.name}</S.GroupName>
+              <S.GroupImage src={group.groupImageUrl} />
+              <S.GroupName>{group.groupNameKo}</S.GroupName>
             </S.GroupInfo>
           ))}
         </S.GroupListContainer>
       </S.ItemContainer>
-      <Button text="완료" onClick={() => navigate(fromPath)} />
+      <Button text="완료" onClick={handleComplete} />
     </S.PageContainer>
   );
 };
