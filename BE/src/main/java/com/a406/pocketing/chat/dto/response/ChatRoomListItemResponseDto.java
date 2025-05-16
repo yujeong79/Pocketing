6745ffd55c0b46e313e3ddee7f21c468ca537d99
김유정 <aabbc0908@naver.com>
2025-05-16 09:23:21 +0900

    FIX: 채팅방 목록 조회 시 마지막 메시지의 시간도 함께 응답
    S12P31A406-206

--- BE/src/main/java/com/a406/pocketing/chat/dto/response/ChatRoomListItemResponseDto.java
+++ BE/src/main/java/com/a406/pocketing/chat/dto/response/ChatRoomListItemResponseDto.java
@@ -6,6 +6,8 @@ import com.a406.pocketing.user.entity.User;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Builder
 public class ChatRoomListItemResponseDto {
@@ -16,6 +18,7 @@ public class ChatRoomListItemResponseDto {
     private Long exchangeId;
     private String imageUrl;
     private String lastMessageContent;
+    private LocalDateTime lastMessageTime;
     private Integer unreadMessageCount;
 
     public static ChatRoomListItemResponseDto of(ChatRoom chatRoom, User receiver, Long postId, Long exchangeId, String imageUrl, ChatMessage lastChatMessage, Integer unreadMessageCount) {
@@ -27,6 +30,7 @@ public class ChatRoomListItemResponseDto {
                 .exchangeId(exchangeId)
                 .imageUrl(imageUrl)
                 .lastMessageContent(lastChatMessage.getMessageContent())
+                .lastMessageTime(lastChatMessage.getCreatedAt())
                 .unreadMessageCount(unreadMessageCount)
                 .build();
     }
