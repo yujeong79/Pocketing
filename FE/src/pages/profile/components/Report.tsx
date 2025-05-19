fc16064981afb7d87470378e98b7aeeeb579269d
한윤지 <hyj9908@gamil.com>
2025-05-19 17:50:31 +0900

    FIX: 마이페이지에 제보하기 연결

--- FE/src/pages/profile/components/Report.tsx
+++ FE/src/pages/profile/components/Report.tsx
@@ -1,9 +1,12 @@
 import * as S from './ReportStyle';
 import { CallIcon } from '@/assets/assets';
+import { useNavigate } from 'react-router-dom';
 
 const Report = () => {
+  const navigate = useNavigate();
+
   return (
-    <S.TitleContainer>
+    <S.TitleContainer onClick={ () => navigate('/report-missing')}>
       <S.TitleIcon src={CallIcon} alt="제보하기" />
       <S.Title>제보하기</S.Title>
     </S.TitleContainer>
