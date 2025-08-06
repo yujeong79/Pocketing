3199e2edb4781e989f2ef0cc7dd5bb04017824be
한병현 <gksqudgus777@naver.com>
2025-08-06 23:05:48 +0900

    FEAT: 로그인 버튼 규정에 맞게 구현

--- MOBILE/src/constants/icons.ts
+++ MOBILE/src/constants/icons.ts
@@ -3,9 +3,19 @@ import { ImageRequireSource } from "react-native";
 const LOGO_3D: ImageRequireSource = require('../../assets/icons/3D_logo.png');
 const LOGO_3D_X2: ImageRequireSource = require('../../assets/icons/3D_logo_x2.png');
 const LOGO_3D_X3: ImageRequireSource = require('../../assets/icons/3D_logo_x3.png');
+
 const LOGO_TEXT: ImageRequireSource = require('../../assets/icons/Text_logo.png');
 const LOGO_TEXT_X2: ImageRequireSource = require('../../assets/icons/Text_logo_x2.png');
 const LOGO_TEXT_X3: ImageRequireSource = require('../../assets/icons/Text_logo_x3.png');
+
+const KAKAO_LOGIN_BUTTON: ImageRequireSource = require('../../assets/icons/Kakao_login.png');
+const KAKAO_LOGIN_BUTTON_X2: ImageRequireSource = require('../../assets/icons/Kakao_login_x2.png');
+const KAKAO_LOGIN_BUTTON_X3: ImageRequireSource = require('../../assets/icons/Kakao_login_x3.png');
+const KAKAO_LOGIN_BUTTON_X4: ImageRequireSource = require('../../assets/icons/kakao_login_large_wide.png');
+
+const X_LOGIN_BUTTON: ImageRequireSource = require('../../assets/icons/X_login.png');
+const X_LOGIN_BUTTON_X2: ImageRequireSource = require('../../assets/icons/X_login_x2.png');
+const X_LOGIN_BUTTON_X3: ImageRequireSource = require('../../assets/icons/X_login_x3.png');
 
 export {
     LOGO_3D,
@@ -14,4 +24,11 @@ export {
     LOGO_TEXT,
     LOGO_TEXT_X2,
     LOGO_TEXT_X3,
+    KAKAO_LOGIN_BUTTON,
+    KAKAO_LOGIN_BUTTON_X2,
+    KAKAO_LOGIN_BUTTON_X3,
+    KAKAO_LOGIN_BUTTON_X4,
+    X_LOGIN_BUTTON,
+    X_LOGIN_BUTTON_X2,
+    X_LOGIN_BUTTON_X3,
 };
