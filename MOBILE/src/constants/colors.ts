9ceda146bd2a60c5dfc57c8e47002ff2cc4d9970
한병현 <gksqudgus777@naver.com>
2025-08-13 22:24:27 +0900

    FEAT: 스타일 가이드 색상

---
+++ MOBILE/src/constants/colors.ts
@@ -0,0 +1,32 @@
+export const PrimaryColors = {
+    '50': '#FEF0F0',
+    '100': '#FDE9E9',
+    '200': '#FBD1D1',
+    '300': '#F26A6C',
+    '400': '#DA5F61',
+    '500': '#C25556',
+    '600': '#B65051',
+    '700': '#914041',
+    '800': '#6D3031',
+    '900': '#552526',
+  };
+  
+  export const SystemColors = {
+    danger: '#C22B20',
+    warning: '#FFE949',
+    success: '#00C380',
+    info: '#4FA5FF',
+  };
+  
+  export const GrayColors = {
+    white: '#FFFFFF',
+    '100': '#F5F5F5',
+    '200': '#E0E0E0',
+    '300': '#CCCCCC',
+    '400': '#AFAFAF',
+    '500': '#8C8C8C',
+    '600': '#6B6B6B',
+    '700': '#4E4E4E',
+    '800': '#2C2C2C',
+    black: '#000000',
+  };
