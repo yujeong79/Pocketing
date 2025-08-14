f5f235e391cda412738bc1ccdba9df77ac7cf2c6
한병현 <gksqudgus777@naver.com>
2025-08-15 00:23:22 +0900

    FEAT: Root View 필수 컴포넌트 적용

--- MOBILE/App.tsx
+++ MOBILE/App.tsx
@@ -1,7 +1,16 @@
 import RootNavigator from './src/navigation/RootNavigator';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { RootSiblingParent } from 'react-native-root-siblings';
 
 export default function App() {
   return (
-    <RootNavigator />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider>
+        <RootSiblingParent>
+          <RootNavigator />
+        </RootSiblingParent>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
--- MOBILE/src/navigation/OnboardingStackNavigator.tsx
+++ MOBILE/src/navigation/OnboardingStackNavigator.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
 import SplashScreen from '@screens/Onboarding/SplashScreen';
 import SignInScreen from '@screens/Onboarding/SignInScreen';
 import NicknameScreen from '@screens/Onboarding/NicknameScreen';
+import { GrayColors } from '@constants/colors';
 
 export type OnboardingStackParamList = {
   Splash: undefined;
@@ -14,16 +16,21 @@ const OnboardingStackNavigator = () => {
   const Stack = createNativeStackNavigator<OnboardingStackParamList>();
 
   return (
-    <Stack.Navigator initialRouteName="Splash">
+    <Stack.Navigator 
+      initialRouteName="Splash" 
+      screenOptions={{
+        headerShown: false,
+        contentStyle: {
+          backgroundColor: GrayColors.white,
+        }
+    }}>
       <Stack.Screen
         name="Splash"
         component={SplashScreen}
-        options={{headerShown: false}}
       />
       <Stack.Screen
         name="SignIn"
         component={SignInScreen}
-        options={{headerShown: false}}
       />
       <Stack.Screen
         name="Nickname"
