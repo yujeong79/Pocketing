e9dc50df7cf698014ffebcb71188571c495e0ea9
김유정 <aabbc0908@naver.com>
2025-09-18 20:57:51 +0900

    FIX: fix CORS setAllowedOrigins

--- BE/src/main/java/com/a406/pocketing/common/config/SecurityConfig.java
+++ BE/src/main/java/com/a406/pocketing/common/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173", "https://k12a406.p.ssafy.io"));
+        config.setAllowedOrigins(List.of("http://localhost:5173", "https://pocketing.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("*"));
